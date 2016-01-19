c0fbb5af58075a2591a8a897d242283b8c57725d
kwangsik <lks21c@gmail.com>
2016-01-19 14:18:06 +0900

    Support Spring Dispatcher Servlet

--- src/main/java/com/creamsugardonut/SleepingClassFileTransformer.java
+++ src/main/java/com/creamsugardonut/SleepingClassFileTransformer.java
@@ -16,15 +16,27 @@ public class SleepingClassFileTransformer implements ClassFileTransformer {
 	public byte[] transform(ClassLoader loader, String className, Class classBeingRedefined,
 			ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
 		byte[] byteCode = classfileBuffer;
-		if (className.equals("com/creamsugardonut/Sleeping")) {
+		if (className.equals("org/springframework/web/servlet/DispatcherServlet")) {
 			try {
 				ClassPool cp = ClassPool.getDefault();
-				CtClass cc = cp.get("com.creamsugardonut.Sleeping");
-				CtMethod m = cc.getDeclaredMethod("randomSleep");
-				m.addLocalVariable("elapsedTime", CtClass.longType);
-				m.insertBefore("elapsedTime = System.currentTimeMillis();");
-				m.insertAfter("{elapsedTime = System.currentTimeMillis() - elapsedTime;"
-						+ "System.out.println(\"Method Executed in ms: \" + elapsedTime);}");
+				cp.importPackage("java.util.Enumeration");
+				CtClass cc = cp.get("org.springframework.web.servlet.DispatcherServlet");
+				CtMethod m = cc.getDeclaredMethod("doService");
+
+                System.out.println("transform");
+
+				StringBuilder sb = new StringBuilder();
+				sb.append(
+						"System.out.println(\"Agent URI: \" + request.getRequestURL() + \"?\"+ request.getQueryString());");
+				sb.append("Enumeration headerValues = request.getHeaderNames();");
+				sb.append("while (headerValues.hasMoreElements()) {");
+				sb.append("String headerName = (String) headerValues.nextElement();");
+				sb.append(
+						"System.out.println(\"Agent Header: \" + headerName + \" = :\" + request.getHeader(headerName));");
+				sb.append("}");
+
+				m.insertAfter(sb.toString());
+
 				byteCode = cc.toBytecode();
 				cc.detach();
 			} catch (Exception ex) {
